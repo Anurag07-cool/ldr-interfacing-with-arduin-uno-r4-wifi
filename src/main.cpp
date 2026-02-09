21c70ae33f14aa36ad78a94d823a1fb8f4fc5f86
Anurag Chaudhary <anurag.choudhary_cs.aiml24@gla.ac.in>
2026-02-09 09:05:24 +0530

    Add void loop and void Status

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <Arduino.h>
+#define ldrPin A0
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
+    pinMode(ldrPin, INPUT);
 }
 
 void loop() {
 // write your code here
+    int ldrValue = analogRead(ldrPin);
+    int lightPercent = map(ldrValue, 0, 1023, 0, 100);
+    Serial.println(ldrValue);
+    Serial.println(lightPercent);
+    delay(500);
 }
